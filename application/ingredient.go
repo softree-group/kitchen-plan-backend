84f1cc993b05f04da0a7620d62f038593fde0dfc
Michael Truschelev <mishele20@mail.ru>
2021-05-03 13:34:58 +0300

    upd

--- application/ingredient.go
+++ application/ingredient.go
@@ -3,6 +3,6 @@ package application
 import "github.com/softree-group/kitchen-plan-backend/domain/entity"
 
 type appIngredientReceiver interface {
-	etAllIngredients(title string) ([]entity.Ingredient, error)
+	GetAllIngredients(title string) ([]entity.Ingredient, error)
 	GetIngredient(id int) (*entity.Ingredient, error)
 }
